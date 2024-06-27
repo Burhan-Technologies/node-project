d0e8b1abc2cfd8db446823698fa930493b0f5201
Burhan-Technologies <khanmuhammadburhan61@gmail.com>
2024-06-27 05:49:59 -0700

    Add files via upload

---
+++ money exchange/money.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+let currencyvalue = {
+    PKR: 281.08,
+    SAR: 3.75,
+    UAE: 3.67,
+    USD: 1
+};
+let answer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        message: "converting from",
+        choices: ["PKR", "SAR", "UAE", "USD"]
+    },
+    {
+        type: "list",
+        name: "to",
+        message: "converting to",
+        choices: ["PKR", "SAR", "UAE", "USD"]
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Your amount to convert"
+    }
+]);
+console.log(currencyvalue[answer.to] / currencyvalue[answer.from] * answer.amount);
