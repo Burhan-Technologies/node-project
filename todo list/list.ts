7c18165002eb724652bbd19bde3bcd957a5986ad
Burhan-Technologies <khanmuhammadburhan61@gmail.com>
2024-06-27 05:52:16 -0700

    Add files via upload

---
+++ todo list/list.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer"
+let todos =[];
+let condition =true;
+
+while(condition)
+
+{
+    let todoQuestion = await inquirer.prompt(
+    [
+
+    {
+        name:"firstQuestion",
+        type:"input",
+        message:"what you want to add in your todos?"
+
+    },
+    {
+        name:"SecondQuestion",
+        type:"confirm",
+        message:"Do you want to add more?",
+        default:"true"
+    }
+    ]
+);
+todos.push(todoQuestion.firstQuestion);
+console.log(todos)
+condition=todoQuestion.SecondQuestion
+}
+
+
+
